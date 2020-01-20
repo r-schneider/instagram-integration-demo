e38b6fef5986b8f56124c0fa510fa86208c16c45
Rodrigo Monte Schneider <rodrigo.schneider@dbccompany.com.br>
2020-01-20 13:29:23 -0300

    feat:(api)added api skeleton

---
+++ src/main/java/com/instagramapi/api/igcomments/IgcommentsApplication.java
@@ -0,0 +1,13 @@
+package com.instagramapi.api.igcomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgcommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgcommentsApplication.class, args);
+	}
+
+}
